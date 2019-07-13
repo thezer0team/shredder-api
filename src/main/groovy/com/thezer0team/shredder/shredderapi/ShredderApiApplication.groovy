4f4426f9314dd3f60cc6bec99052591a87863552
nwill30 <techexpressnw@gmail.com>
2019-07-13 17:06:18 -0500

    Spring init

---
+++ src/main/groovy/com/thezer0team/shredder/shredderapi/ShredderApiApplication.groovy
@@ -0,0 +1,13 @@
+package com.thezer0team.shredder.shredderapi
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class ShredderApiApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(ShredderApiApplication, args)
+	}
+
+}
