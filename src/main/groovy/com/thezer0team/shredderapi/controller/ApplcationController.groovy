bb38c774fa86ba28cbfa0c25ddbac08e1f48c6ab
nwill30 <techexpressnw@gmail.com>
2019-07-20 17:47:07 -0500

    Update ApplcationController.groovy

--- src/main/groovy/com/thezer0team/shredderapi/controller/ApplcationController.groovy
+++ src/main/groovy/com/thezer0team/shredderapi/controller/ApplcationController.groovy
@@ -6,12 +6,16 @@ import org.springframework.web.bind.annotation.RequestMethod
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-@RequestMapping("health/")
+@RequestMapping('')
 class ApplcationController {
 
     @RequestMapping(value = 'shredder', method = RequestMethod.GET)
     ResponseEntity<String> getRetailProduct() {
-
         return ResponseEntity.ok('Tonight I dine on turtle soup.')
+    }
+
+    @RequestMapping(value = '', method = RequestMethod.GET)
+    ResponseEntity<String> helloWorld() {
+        return ResponseEntity.ok('There will be no mistake this time. I go myself.')
     }
 }
