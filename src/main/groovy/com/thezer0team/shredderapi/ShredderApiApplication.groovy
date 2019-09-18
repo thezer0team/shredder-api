c272b802ffe14aafc89cc254a44c2fc96307b809
nwill30 <techexpressnw@gmail.com>
2019-09-18 18:37:15 -0500

    Trying again

--- src/main/groovy/com/thezer0team/shredderapi/ShredderApiApplication.groovy
+++ src/main/groovy/com/thezer0team/shredderapi/ShredderApiApplication.groovy
@@ -1,10 +1,13 @@
 package com.thezer0team.shredderapi
 
+import com.thezer0team.shredderapi.config.DynamoDBConfig
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Import
 
 @Configuration
+//@Import([DynamoDBConfig.class])
 @SpringBootApplication
 class ShredderApiApplication {
 
--- src/main/groovy/com/thezer0team/shredderapi/config/DynamoDBConfig.groovy
+++ src/main/groovy/com/thezer0team/shredderapi/config/DynamoDBConfig.groovy
@@ -9,19 +9,24 @@ import com.amazonaws.services.dynamodbv2.AmazonDynamoDB
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapperConfig
-import com.amazonaws.services.dynamodbv2.document.DynamoDB
 import com.thezer0team.shredderapi.repository.ApplicationCalendarRepository
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration
+import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration
+import org.socialsignin.spring.data.dynamodb.mapping.DynamoDBMappingContext
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration
 import com.thezer0team.shredderapi.repository.PlatformCalendarRepository
 import com.thezer0team.shredderapi.repository.UserRepository
 import org.socialsignin.spring.data.dynamodb.repository.config.EnableDynamoDBRepositories
 import org.springframework.beans.factory.annotation.Value
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration
 import org.springframework.boot.autoconfigure.domain.EntityScan
 import org.springframework.context.annotation.Bean
-import org.springframework.context.annotation.ComponentScan
 import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Primary
 
 @Configuration
-@EnableDynamoDBRepositories(basePackageClasses = [PlatformCalendarRepository.class, ApplicationCalendarRepository.class, UserRepository.class])
+@EnableAutoConfiguration(exclude = [DataSourceAutoConfiguration.class, DataSourceTransactionManagerAutoConfiguration.class, HibernateJpaAutoConfiguration.class])
+@EnableDynamoDBRepositories(basePackageClasses =  [ApplicationCalendarRepository.class, PlatformCalendarRepository.class, UserRepository.class])
 @EntityScan(basePackages = 'com.thezer0team.shredderapi.*.*')
 class DynamoDBConfig {
 
@@ -30,6 +35,10 @@ class DynamoDBConfig {
 
     @Value('${amazon.dynamodb.secretkey}')
     private String amazonDynamoDBSecretKey
+
+    AWSCredentialsProvider amazonAWSCredentialsProvider() {
+        return new AWSStaticCredentialsProvider(amazonAWSCredentials())
+    }
 
     @Bean
     AWSCredentials amazonAWSCredentials() {
@@ -42,26 +51,20 @@ class DynamoDBConfig {
     }
 
     @Bean
+    @Primary
     DynamoDBMapper dynamoDBMapper(AmazonDynamoDB amazonDynamoDB, DynamoDBMapperConfig config) {
         return new DynamoDBMapper(amazonDynamoDB, config)
     }
 
     @Bean
     AmazonDynamoDB amazonDynamoDB() {
-        return AmazonDynamoDBClientBuilder
-                .standard()
-                .withCredentials(amazonAWSCredentialsProvider())
-                .withRegion(Regions.US_EAST_2)
-                .build()
+        return AmazonDynamoDBClientBuilder.standard().withCredentials(amazonAWSCredentialsProvider())
+                .withRegion(Regions.US_EAST_1).build()
     }
 
     @Bean
-    DynamoDB dynamoDB() {
-        return new DynamoDB(amazonDynamoDB())
-    }
-
-    AWSCredentialsProvider amazonAWSCredentialsProvider() {
-        return new AWSStaticCredentialsProvider(amazonAWSCredentials())
+    DynamoDBMappingContext dynamoDBMappingContext() {
+        return new DynamoDBMappingContext()
     }
 }
 
